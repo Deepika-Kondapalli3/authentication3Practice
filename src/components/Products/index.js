194da57f74cecae3e358b4426d5629ee7136953d
Deepika-Kondapalli3 <kondapallideepika03@gmail.com>
2024-07-07 10:12:19 +0000

    1st

---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import AllProductsSection from '../AllProductsSection'
+
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => {
+  return (
+    <>
+      <Header />
+      <div className="product-sections">
+        <AllProductsSection />
+      </div>
+    </>
+  )
+}
+
+export default Products
